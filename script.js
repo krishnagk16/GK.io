6a08933376d2cc8d31994be957104df060565a04
krishnagk16 <krishkrishnankm@gmail.com>
2025-11-23 20:13:38 +0530

    update

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+/*
 function openLogin() {
   document.getElementById("loginbox").style.display = "block";
 }
@@ -7,3 +9,24 @@ function closeLogin() {
 }
 
 document.getElementById("closeBtn").onclick = closeLogin;
+
+*/
+        function openLogin() {
+            const box = document.getElementById("loginbox");
+            box.style.display = "inline-block";
+            setTimeout(() => {
+                box.classList.add("show");
+                box.classList.remove("hide");
+            }, 10);
+        }
+
+        function closeLogin() {
+            const box = document.getElementById("loginbox");
+            box.classList.add("hide");
+            box.classList.remove("show");
+            setTimeout(() => {
+                box.style.display = "none";
+            }, 800);
+        }
+
+        document.getElementById("closeBtn").onclick = closeLogin;
